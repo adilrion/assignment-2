7f7e14af63aff1d3d4e6693ab30b27c0c77563a6
adilrion <adilmahmoudrion@gmail.com>
2024-05-21 03:46:28 +0600

    update inventory based on order

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import productRouter from '../modules/product/product.route'
+import orderRouter from '../modules/order/order.route'
 
 const applicationRouter = express.Router()
 
@@ -8,6 +9,10 @@ const allRouters = [
     path: '/products',
     route: productRouter,
   },
+  {
+    path: '/orders',
+    route: orderRouter
+  }
 ]
 
 allRouters?.forEach(route => {
